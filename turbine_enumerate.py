b389568689fe7dcdd804a0ab5311ba79f2e39c8f
idukkigold <nithish.nitt@gmail.com>
2018-11-09 12:17:34 -0500

    Added Enumerate

--- turbine_enumerate.py
+++ turbine_enumerate.py
@@ -3,21 +3,37 @@ r=10000 #Given
 
 from itertools import permutations
 weight=[i+1 for i in range(10)]
-s=[]
+
+W=sum(weight)
+clist=[]
 
 #Excluding an item and Permuting different combinations
 for i in permutations(weight[0:9],9):
-    s.append(list(i))
+    clist.append(list(i))
 
 #Adding the combination to the fixed element
-for i in s:
+for i in clist:
     i.append(10)
-    print(i)
 
 #Finding the CoG of each solution
-for i,solution in enumerate(s):
-    #function to generate distance
-    pass
+import math
+
+
+
+def getCG(solution_list):
+    n=len(solution_list)
+    Xcg=0
+    Ycg=0
+
+    for sln in solution_list:
+
+        pass
+
+
+
+
+
+
 
 
 

005000354af197aa6db4158aaae9f93801865c2f
idukkigold <nithish.nitt@gmail.com>
2018-11-11 10:57:29 -0500

    Added Enumerate

--- turbine_enumerate.py
+++ turbine_enumerate.py
@@ -1,57 +1,66 @@
-r=10000 #Given
+r = 10000  # Given
 
 from itertools import permutations
-from math import cos,sin,pi,sqrt
-import itertools
+from math import cos, sin, pi, sqrt
 
-weight=[i+1 for i in range(10)]
-W=sum(weight)
-clist=[]
+N = int(input("Enter no of turbines: "))
 
-#Excluding an item and Permuting different combinations
+weight = [i + 1 for i in range(N)]
+W = sum(weight)
+clist = []
 
-for i in permutations(weight[0:9],9):
+# Excluding an item and Permuting different combinations
+
+for i in permutations(weight[0:N - 1], N - 1):
     clist.append(list(i))
 
-#Adding the combination to the fixed element
+# Adding the combination to the fixed element
 
 for i in clist:
-    i.append(10)
+    i.append(weight[-1])
+    print(i)
 
-#Finding the CoG of each solution
-xlist=[]
-ylist=[]
+# Finding the CoG of each solution
+xlist = []
+ylist = []
+
 
 def getCG(solution_vector):
-    n=len(solution_vector)
-    Xcg=0
-    Ycg=0
+    n = len(solution_vector)
+    Xcg = 0
+    Ycg = 0
 
-    for i,sln in enumerate(solution_vector):
+    for i, sln in enumerate(solution_vector):
         # print(sln)
-        Xcg=Xcg+r*cos(2*pi*i/n)*sln
-        Ycg=Ycg+r*sin(2*pi*i/n)*sln
+        Xcg = Xcg + r * cos(2 * pi * i / n) * sln
+        Ycg = Ycg + r * sin(2 * pi * i / n) * sln
 
-    Xcg/=W
-    Ycg/=W
+    Xcg /= W
+    Ycg /= W
 
     xlist.append(Xcg)
     ylist.append(Ycg)
 
+    print(Xcg, Ycg)
 
-    print(Xcg,Ycg)
 
 for sln in clist:
     getCG(sln)
 
-xlist2=[i*i for i in xlist]
-ylist2=[i*i for i in ylist]
+xlist2 = [i * i for i in xlist]
+ylist2 = [i * i for i in ylist]
 
-for i,j in zip(xlist2,ylist2):
-    print(sqrt(i+j))
+d_min = sqrt(xlist2[0] + ylist2[0])
+a = 0
+for i, j in zip(xlist2, ylist2):
+    d = sqrt(i + j)
+    if d < d_min:
+        d_min = d
+        a += 1
 
+    print(sqrt(i + j))
 
-
+print("Minimum Deviation: ",clist[a],"\n", d_min)
 
 
 

9ec69f048efbe320a3134e344b8d1971860ccc07
idukkigold <nithish.nitt@gmail.com>
2018-11-26 17:59:53 -0500

    -import

--- Turbine.py
+++ Turbine.py
@@ -1,14 +1,12 @@
 import numpy as np
 from itertools import  permutations
+from collections import Counter
 import timeit
 from math import cos, sin, pi, sqrt,fabs
 
-
+start = timeit.default_timer()
 n= int (input(" Enter turbine count: "))
-
-
-
-
+weight = [i + 1 for i in range(n)]
 
 
 #fix slots 0 for open 1 for placed
@@ -16,17 +14,19 @@ Slots= [0 for i in range(n-1)]
 Slots.append(1)
 Slots.reverse()
 
-Current_config=[]
+W = sum(weight)
+Current_config=Slots
 
 def Cal_MinSlot(Current_config):
+    k=Current_config.count(1)
+    print(k)
 
-    minSlot=1
-
-    return minSlot
+    # return minSlot
 
 
 def PlaceTurbine():
-
     while 0 not in Current_config:
         minSlot=Cal_MinSlot(Current_config)
-        Current_config[minSlot]=1
+        Current_config[minSlot] = 1
+
+Cal_MinSlot(Current_config)

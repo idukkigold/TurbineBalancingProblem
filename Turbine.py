3660657b263ee3388cd7dff48b6286ac400be4c7
idukkigold <nithish.nitt@gmail.com>
2018-11-30 13:10:51 -0500

    Initial Solution

--- Turbine.py
+++ Turbine.py
@@ -28,9 +28,9 @@ def PlaceTurbine():
     n = int(input(" Enter turbine count: "))
     weight = [i + 1 for i in range(n)]
 
-    # fix slots 0 for open 1 for placed
+    # fix slots '0' for open, '1' for placed
     Slots = [0 for i in range(n - 1)]
-    Slots.append(1)
+    Slots.append(n)
     Slots.reverse()
     W = np.sum(sum(weight))
     Current_config = Slots

cb28bc5eb032124b3193d2c30b26bfd8383a332e
idukkigold <nithish.nitt@gmail.com>
2018-12-02 19:48:20 -0500

    Optmised enumerate

--- comb.py
+++ comb.py
@@ -1,26 +1,64 @@
+from itertools import permutations
+from math import cos, sin, pi, sqrt,inf
+import timeit
+
+r = 1000  # Given
+
+N = int(input("Enter no of turbines: "))
+
+start = timeit.default_timer()
+weight = [i + 1 for i in range(N)]
+W = sum(weight)
+clist = []
+
+# Excluding an item and Permuting different combinations
+
+for i in permutations(weight[0:N - 1], N - 1):
+    clist.append(list(i))
+
+# Adding the combination to the fixed element
+
+for i in clist:
+    i.append(weight[-1])
+    # print(i)
+
+# Finding the CoG of each solution
+S=0
+d_min=inf
+
+def getCG(solution_vector):
+    n = len(solution_vector)
+    Xcg = 0
+
+    Ycg = 0
+    global d_min,S
+
+    for i, sln in enumerate(solution_vector):
+        # print(sln)
+        Xcg = Xcg + r * cos(2 * pi * i / n) * sln
+        Ycg = Ycg + r * sin(2 * pi * i / n) * sln
+
+    Xcg /= W
+    Ycg /= W
+    xlist2 = Xcg**2
+    ylist2 = Ycg**2
+    d = sqrt(xlist2 + ylist2)
+
+    if d<d_min:
+            S=solution_vector
+            d_min=d
+            # print(d, solution_vector)
+
+
+for i in clist:
+    getCG(i)
+
+print("Minimum Deviation: ", d_min," at ",S,"\n")
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)
 
 
 
-def get_cyclics(p, k):
-  found = set()      # set of tuples we have seen so far
-  todo = [tuple(p)]  # list of tuples we still need to explore
-  n = len(p)
-  while todo:
-    x = todo.pop()
-    for i in range(n - k + 1):
-      perm = ( x[:i]                    # Prefix
-             + x[i+1:i+k] + x[i:i+1]    # Rotated middle
-             + x[i+k:]                  # Suffix
-             )
-      if perm not in found:
-        found.add(perm)
-        todo.append(perm)
-  for x in found:
-    print(x)
 
-
-k=2
-p=[1,2,3,4]
-
-get_cyclics(p,2)
 
